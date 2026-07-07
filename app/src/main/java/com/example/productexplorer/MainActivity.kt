cfd712e55edfc580ec1a71103c62d92f156acb23
Ahmed <letaiefahmedyahya@gmail.com>
2026-07-07 14:47:52 +0200

    feat: ajouter un espace sous l'en-tete avec Spacer

--- app/src/main/java/com/example/productexplorer/MainActivity.kt
+++ app/src/main/java/com/example/productexplorer/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
@@ -58,6 +60,7 @@ fun ProductHomeScreen(
             .padding(16.dp)
     ) {
         HomeHeader()
+        Spacer(modifier = Modifier.height(24.dp))
     }
 }
 
