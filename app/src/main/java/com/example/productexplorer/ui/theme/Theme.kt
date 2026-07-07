edf3e65bedd6616f19a0f7cadc84e43be037f0fb
Ahmed <letaiefahmedyahya@gmail.com>
2026-07-07 17:29:07 +0200

    chore: retirer un import inutilise (Activity) dans Theme.kt

--- app/src/main/java/com/example/productexplorer/ui/theme/Theme.kt
+++ app/src/main/java/com/example/productexplorer/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.productexplorer.ui.theme
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
